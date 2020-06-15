0420dcb7d07b43c2e6ba73d14805bbc7f9f603f7
Mauricio <eu@massaaki.com>
2020-06-14 21:26:55 -0300

    (RLanguage): Aprendendo comandos básicos e criado estrutura básica de configuraçaõ

---
+++ 01 - Linguagem R/01 - R Language.R
@@ -0,0 +1,18 @@
+# R Language Sample
+
+# To run scripts press Ctrl or Alt + Enter
+
+# Print
+print('Hello world');
+
+
+# Plot Graphcs
+plot(10:100)
+hist(rnorm(20))
+
+
+# packages Install
+install.packages("ggplot2");
+
+# load package
+library(ggplot2);
